1f7d0f95d81dbc9c667cb85a47f65a4c5cd2a0ee
Udoozy <agbo4samson@gmail.com>
2024-11-20 20:24:35 +0000

    0-rotate_2d_matrix.py

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -14,14 +14,14 @@ def rotate_2d_matrix(matrix):
     for i in range(int(n / 2)):
         k = (n - i - 1)
         for j in range(i, k):
-            l = (n - 1 - j)
+            m = (n - 1 - j)
             # the actual number
             tmp = matrix[i][j]
             # changing the top for left
-            matrix[i][j] = matrix[l][i]
+            matrix[i][j] = matrix[m][i]
             # changing left for bottom
-            matrix[l][i] = matrix[k][l]
+            matrix[m][i] = matrix[k][m]
             # changing bottom for right
-            matrix[k][l] = matrix[j][k]
+            matrix[k][m] = matrix[j][k]
             # changing right for top
             matrix[j][k] = tmp
